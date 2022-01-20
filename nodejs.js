17256d9ac90b3da4069da93bd6d5fd368bb72ce7
Shawn McKnight <smcknight@storis.com>
2022-01-20 17:32:30 -0500

    Only use explicit-module-boundary-types on ts files

--- nodejs.js
+++ nodejs.js
@@ -4,9 +4,6 @@ module.exports = {
 	settings: { 'import/resolver': { 'babel-module': { extensions: ['.ts'] } } },
 
 	rules: {
-		// enforce return types on module boundaries
-		'@typescript-eslint/explicit-module-boundary-types': 'error',
-
 		// set up naming convention rules
 		'@typescript-eslint/naming-convention': [
 			'error',
@@ -33,6 +30,14 @@ module.exports = {
 	},
 
 	overrides: [
+		{
+			files: ['**/*.ts'],
+			rules: {
+				// enforce return types on module boundaries
+				'@typescript-eslint/explicit-module-boundary-types': 'error',
+			},
+		},
+
 		{
 			files: ['**/*.types.ts', '**/types/*.ts', '**/*.schema.ts', '**/instances/**'],
 			rules: {
